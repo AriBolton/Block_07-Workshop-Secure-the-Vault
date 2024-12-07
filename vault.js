54088afefe9b932ae294c5e07d83a573da37fc45
aribolton <ari.bolton2011@gmail.com>
2024-12-07 14:00:23 -0800

    submitting assignment

---
+++ vault.js
@@ -0,0 +1,14 @@
+/**Pseudocode:
+ * 1. Create the const numbers for the vault code using uniqe calculations for each
+ * 2. Create the alert string tieing together all variables
+ * 
+ **/
+
+//1. Create the const numbers for the vault code using uniqe calculations for each
+const num1 = 9+1;
+const num2 = 10*4;
+const num3 = 40-1;
+
+//2. Create the alert string tieing together all variables
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${num1} - ${num2} - ${num3}`)
+
